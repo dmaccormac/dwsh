c26e1cf30a78763bbbb87b83b130f11d86618731
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-02-05 19:32:44 +0000

    Add Config.cs

---
+++ dwsh/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace dwsh
+{
+    internal static class Config
+    {
+        public static readonly string DamewareExecutable = "dwrcc.exe";
+        public static readonly string DamewareArguments = "-c: -h: -m:";
+        public static readonly string LogFile = "dwsh.log";
+
+    }
+}
