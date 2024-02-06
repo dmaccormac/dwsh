099d9a2b110fb09f17a4ead4a9003ba84df6b765
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-02-06 16:05:10 +0000

    Update Uninstaller.cs

--- dwsh/Uninstaller.cs
+++ dwsh/Uninstaller.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Win32;
-using static System.Environment;
+﻿using static dwsh.RegistryHelper;
+using static dwsh.PathHelper;
 
 namespace dwsh
 {
@@ -10,39 +10,9 @@ namespace dwsh
             Console.WriteLine("Uninstall dwsh...");
             Console.WriteLine("Directory: " + damewareDirectory);
 
-            //remove registry writes
-            try
-            {
-                Console.Write("Removing registry entries...");
-                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
-                key.DeleteSubKey(".dwc", false);
-                key.DeleteSubKeyTree("DamewareConnection", false);
-                Console.WriteLine("OK");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("ERROR");
-                Console.WriteLine(e.Message);
-            }
-
-            //remove DamewareDirectory from user PATH variable
-            try
-            {
-
-                Console.Write("Removing environment variable entries...");
-
-                string path = GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
-                while (path.Contains(damewareDirectory))
-                    path = path.Replace(";" + damewareDirectory, "");
-
-                Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.User);
-                Console.WriteLine("OK");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("ERROR");
-                Console.WriteLine(e.Message);
-            }
+            DeleteRegistryKey(@"Software\Classes\.dwc");
+            DeleteRegistryKey(@"Software\Classes\DamewareConnection");
+            RemoveEntryFromUserPath(damewareDirectory);
 
         }
     }
