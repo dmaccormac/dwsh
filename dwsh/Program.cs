4fce8f76b4610630f97216125cdc22e0581a9259
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-02-07 21:19:10 +0000

    Update Program.cs

--- dwsh/Program.cs
+++ dwsh/Program.cs
@@ -1,36 +1,58 @@
-﻿using dwsh;
+﻿using System.Text.RegularExpressions;
 
-Shell shell = new();
-
-if (args.Length == 0)
+namespace dwsh
 {
-
-    while (true)
+    internal static class Program
     {
-        Console.Write(shell.Prompt);
-        string[]? input = Console.ReadLine()?.Trim().Split();
-
-        if (input != null) 
+        public static void Main(String[] args)
         {
-            var command = input[0];
-            var arguments = input.Skip(1).ToArray();
-            shell.RunCommand(command, arguments);
+            Shell shell = new();
+
+            if (args.Length == 0)
+            {
+
+                while (true)
+                {
+                    Console.Write(shell.Prompt);
+                    string? input = Console.ReadLine()?.Trim();
+
+                    if (input != null && input.Length > 0)
+                    {
+                        var parsedInput = SplitWithQuotes(input);
+                        var command = parsedInput[0];
+                        var arguments = parsedInput.Skip(1).ToArray();
+                        shell.RunCommand(command, arguments);
+                    }
+                }
+            }
+
+            else
+            {
+
+                var command = args[0];
+                var arguments = args.Skip(1).ToArray();
+                shell.RunCommand(command, arguments);
+
+                if (command == "connect")
+                {
+                    Console.WriteLine(String.Join(" ", args));
+                    Console.Read();
+                }
+
+
+            }
+
+        }
+
+        private static string[] SplitWithQuotes(string input)
+        {
+            return Regex.Matches(input, @"(""|').+?(\1)|[^ ]+")
+                        .Cast<Match>()
+                        .Select(m => m.Value.Trim('"', '\''))
+                        .ToArray();
         }
     }
 }
 
-else
-{
 
-    var command = args[0];
-    var arguments = args.Skip(1).ToArray();
-    shell.RunCommand(command, arguments);
 
-    if (command == "connect")
-    {
-        Console.WriteLine(String.Join(" ", args));
-        Console.Read();
-    }
-        
-
-}
